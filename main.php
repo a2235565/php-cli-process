a0cf4a31a254158e6033bb38604741e756f5c2aa
a2235565 <vs123kkom@qq.com>
2019-01-07 20:22:53 +0800

    封装构造

--- main.php
+++ main.php
@@ -1,13 +1,12 @@
 <?php
 require 'ProcessHelp.php';
 require 'MsgQueue.php';
-
-
+//制造数据
 $list = [];
-for($i=0;$i<100;$i++){
+for($i=0;$i<10;$i++){
     $list[] = 'http://xxx.cn?a='.$i;
 }
-
+//执行任务
 run($list,1,function ($one){
     sleep(1);
     echo $one.PHP_EOL;
@@ -28,30 +27,39 @@ function run($msgList,$number,$callback){
         $msg_queue->push($v);
     }
 //进程守护
-umask(0);
-posix_setsid();
-
-$pid = pcntl_fork();
-if ($pid>0){
-    exit(0);
-}
-file_put_contents(__DIR__.'/pid.log',getmypid());
-$t->setNumber(1);
-$t->process(
-    function (ProcessHelp $_this) {
-        while (  $l = $_this->getMq()->pop(1)) {
-            sleep(1);
-            file_put_contents(__DIR__.'/test.txt',$l."\n\r",FILE_APPEND);
-        }
+    umask(0);
+    posix_setsid();
+    $pid = pcntl_fork();
+    if ($pid>0){
+        exit(0);
     }
-);
-
-pcntl_async_signals(true);
-pcntl_signal(SIGUSR1,function (){
-    //重启 逻辑  mac  kill -30 pid.log   linux kill -10 pid.log
-    file_put_contents(__DIR__.'/tt.log',1);
-});
-
-while (true){
-    usleep(1000);
+    file_put_contents(__DIR__.'/pid.log',getmypid());
+    $t->setNumber($number);
+    $t->process(
+        function (ProcessHelp $_this) use ($callback) {
+            while (  $l = $_this->getMq()->pop(1)) {
+                if(is_callable($callback)){
+                    $callback($l);
+                }
+            }
+        }
+    );
+    pcntl_async_signals(true);
+    pcntl_signal(SIGUSR1,function () use($t,$number,$callback){
+        //重启 逻辑  mac  kill -30 pid.log   linux kill -10 pid.log
+        $t->killAll();
+        $t->setNumber($number);
+        $t->process(
+            function (ProcessHelp $_this) use ($callback) {
+                while (  $l = $_this->getMq()->pop(1)) {
+                    if(is_callable($callback)){
+                        $callback($l);
+                    }
+                }
+            }
+        );
+    });
+    while (true){
+        usleep(1000);
+    }
 }
