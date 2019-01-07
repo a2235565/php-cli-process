044892c42587a1b907086fb91011dc3378a10749
Yzy <vs123kkom@qq.com>
2019-01-07 20:18:08 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,42 +1,73 @@
 <?php
 require 'ProcessHelp.php';
 require 'MsgQueue.php';
-$msg_queue = new MsgQueue();
-$t = new ProcessHelp();
-$t->setMq($msg_queue);
-$msg_queue->push(1);
-$msg_queue->push(3);
-$msg_queue->push(2);
-$msg_queue->push(2);
-$msg_queue->push(2);
-$msg_queue->push(2);
-$msg_queue->push(2);
-$msg_queue->push(2123);
-//进程守护
-umask(0);
-posix_setsid();
 
-$pid = pcntl_fork();
-if ($pid>0){
-    exit(0);
+//制造数据
+$list = [];
+for($i=0;$i<100;$i++){
+    $list[] = 'http://xxx.cn?a='.$i;
 }
-file_put_contents(__DIR__.'/pid.log',getmypid());
-$t->setNumber(1);
-$t->process(
-    function (ProcessHelp $_this) {
-        while (  $l = $_this->getMq()->pop(1)) {
-            sleep(1);
-            file_put_contents(__DIR__.'/test.txt',$l."\n\r",FILE_APPEND);
-        }
-    }
-);
 
-pcntl_async_signals(true);
-pcntl_signal(SIGUSR1,function (){
-    //重启 逻辑  mac  kill -30 pid.log   linux kill -10 pid.log
-    file_put_contents(__DIR__.'/tt.log',1);
+//执行任务
+run($list,1,function ($one){
+    sleep(1);
+    echo $one.PHP_EOL;
 });
 
-while (true){
-    usleep(1000);
+
+
+
+/**
+ * @param $msgList //队列
+ * @param $number //开启进程数量
+ * @param $callback //回调函数
+ */
+function run($msgList,$number,$callback){
+    $msg_queue = new MsgQueue();
+    $t = new ProcessHelp();
+    $t->setMq($msg_queue);
+    foreach ($msgList as $v)
+    {
+        $msg_queue->push($v);
+    }
+//进程守护
+    umask(0);
+    posix_setsid();
+
+    $pid = pcntl_fork();
+    if ($pid>0){
+        exit(0);
+    }
+    file_put_contents(__DIR__.'/pid.log',getmypid());
+    $t->setNumber($number);
+    $t->process(
+        function (ProcessHelp $_this) use ($callback) {
+            while (  $l = $_this->getMq()->pop(1)) {
+                if(is_callable($callback)){
+                    $callback($l);
+                }
+            }
+        }
+    );
+
+    pcntl_async_signals(true);
+    pcntl_signal(SIGUSR1,function () use($t,$number,$callback){
+        //重启 逻辑  mac  kill -30 pid.log   linux kill -10 pid.log
+        $t->killAll();
+        $t->setNumber($number);
+        $t->process(
+            function (ProcessHelp $_this) use ($callback) {
+                while (  $l = $_this->getMq()->pop(1)) {
+                    if(is_callable($callback)){
+                        $callback($l);
+                    }
+                }
+            }
+        );
+    });
+
+    while (true){
+        usleep(1000);
+    }
+
 }
