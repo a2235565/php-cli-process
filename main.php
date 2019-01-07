3b958e8be7ab1c8fdd4c2ecf547604d3261f7da6
Yzy <vs123kkom@qq.com>
2019-01-07 17:59:53 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,11 +1,9 @@
 <?php
 require 'ProcessHelp.php';
 require 'MsgQueue.php';
-
 $msg_queue = new MsgQueue();
 $t = new ProcessHelp();
 $t->setMq($msg_queue);
-//队列
 $msg_queue->push(1);
 $msg_queue->push(3);
 $msg_queue->push(2);
@@ -13,22 +11,32 @@ $msg_queue->push(2);
 $msg_queue->push(2);
 $msg_queue->push(2);
 $msg_queue->push(2);
-$msg_queue->push(2);
-//关闭终端后继续执行代码
+$msg_queue->push(2123);
+//进程守护
 umask(0);
 posix_setsid();
-//end
-//设置进程数
+
+$pid = pcntl_fork();
+if ($pid>0){
+    exit(0);
+}
+file_put_contents(__DIR__.'/pid.log',getmypid());
 $t->setNumber(1);
-//end
 $t->process(
     function (ProcessHelp $_this) {
         while (  $l = $_this->getMq()->pop(1)) {
             sleep(1);
-            var_dump($l);
+            file_put_contents(__DIR__.'/test.txt',$l."\n\r",FILE_APPEND);
         }
     }
 );
 
-exit(0);
+pcntl_async_signals(true);
+pcntl_signal(SIGUSR1,function (){
+    //重启 逻辑  mac  kill -30 pid.log   linux kill -10 pid.log
+    file_put_contents(__DIR__.'/tt.log',1);
+});
 
+while (true){
+    usleep(1000);
+}
