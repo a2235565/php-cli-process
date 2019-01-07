7a9a4d24185ab87c36f690f364ea007be98a88bb
Yzy <vs123kkom@qq.com>
2019-01-07 17:42:09 +0800

    Update main.php

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ require 'MsgQueue.php';
 $msg_queue = new MsgQueue();
 $t = new ProcessHelp();
 $t->setMq($msg_queue);
+//队列
 $msg_queue->push(1);
 $msg_queue->push(3);
 $msg_queue->push(2);
@@ -13,8 +14,13 @@ $msg_queue->push(2);
 $msg_queue->push(2);
 $msg_queue->push(2);
 $msg_queue->push(2);
+//关闭终端后继续执行代码
 umask(0);
 posix_setsid();
+//end
+//设置进程数
+$t->setNumber(1);
+//end
 $t->process(
     function (ProcessHelp $_this) {
         while (  $l = $_this->getMq()->pop(1)) {
