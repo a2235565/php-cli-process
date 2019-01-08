7f99ceb8effd10451a86db7c0a995bf22226586b
a2235565 <vs123kkkom@qq.com>
2019-01-08 13:35:04 +0800

    格式调整

--- Task.php
+++ Task.php
@@ -14,33 +14,33 @@ class Task
      * @param $number //开启进程数量
      * @param $callback //回调函数
      */
-  static function run($msgList,$number,$callback){
+    static function run($msgList, $number, $callback)
+    {
         $msg_queue = new MsgQueue();
         $t = new ProcessHelp();
         $t->setMq($msg_queue);
 
-        file_put_contents(__DIR__.'/pid.log',getmypid());
+        file_put_contents(__DIR__ . '/pid.log', getmypid());
         $t->setNumber($number);
         $t->process(
-            function (ProcessHelp $_this) use ($callback)  {
-                while ( true ) {
+            function (ProcessHelp $_this) use ($callback) {
+                while (true) {
                     $l = $_this->getMq()->pop(1);
-                    if(is_callable($callback)){
+                    if (is_callable($callback)) {
                         $callback($l);
                     }
                 }
             }
         );
 
-        foreach ($msgList as $v)
-        {
+        foreach ($msgList as $v) {
             $msg_queue->push($v);
         }
 
-        while (true){
+        while (true) {
             sleep(1);
             $status = msg_stat_queue($msg_queue->queue);
-            if($status['msg_qnum']==0){
+            if ($status['msg_qnum'] == 0) {
                 echo 'task is success';
                 $t->killAll();
                 die(0);
