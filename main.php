4d251a315117e9882544432f378cb44b8aab44b7
a2235565 <vs123kkkom@qq.com>
2019-01-08 16:39:35 +0800

    实例

--- main.php
+++ main.php
@@ -3,6 +3,7 @@ require 'ProcessHelp.php';
 require 'MsgQueue.php';
 require 'Daemon.php';
 require 'Task.php';
+require 'Curl.php';
 //监听信号
 Daemon::listenSign();
 //进程守护
@@ -14,19 +15,18 @@ Daemon::setSigUser1Callback(function (){
 
 //制造测试队列
 $list = [];
-for($i=0;$i<100;$i++){
-    $list[] = 'http://xxx.cn?a='.$i;
+for($i=0;$i<200;$i++){
+    $list[] = 'http://www.php20.cn/article/'.$i;
 }
 //进程数量
-$number = 5;
+$number = 1;
 //任务主体
 $callback = function ($one,MsgQueue $MsgQueue){
     sleep(1);
-    echo $one.PHP_EOL;
-    file_put_contents(__DIR__.'/test.log',$one,FILE_APPEND);
-    echo getmypid().PHP_EOL;
-    //假设业务逻辑出错塞回队列
-    if(false){
+    $fileName = explode('/',$one);
+    if($rs = Curl::run($one)){
+        file_put_contents(__DIR__.'/A/'.$fileName[count($fileName)-1].'.html',$rs);
+    }else{
         $MsgQueue->push($one,1);
     }
 };
